d2127c4d1478bbebfa7b81f8c342f346b755e575
exauceanitii <exauceaniti@gmail.com>
2025-08-08 17:56:04 +0200

    Designe de l'index.php

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
     $images = [
-        'pneu.png', 'huile.png', 'bougie.jpg',
-        'porte.jpg', 'volant.jpg',
-        'thermostat.jpg', 'essuie_glace.jpeg',
-        'joint.jpeg', 'plaquette_frein.jpg'
+        'pneu.png' => 'Pneu',
+        'huile.png' => 'Bidon d\'huile ',
+        'bougie.jpg'=> 'Bougies',
+        'essuie_glace.jpeg'=> 'Essuie_Glace',
+        'joint.jpeg'=> 'Joint',
+        'plaquette_frein.jpg'=> 'Plaquette de Frein',
+        'porte.jpg'=> 'Porte',
+        'thermostat.jpg'=> 'Thermostat',
+        'volant.jpg'=> 'Volant',
     ];
+    $keys = array_keys($images);
     shuffle($images);
 ?>
 <!DOCTYPE html>
@@ -17,14 +23,17 @@
 <body>
     <h1>Le Garage de Exauce</h1>
 
-    <table>
-        <tr>
-            <?php
-            for ($i = 0; $i < 3; $i++) {
-                echo '<td><img src="image/' . $images[$i] . '" alt="Image ' . ($i+1) . '"></td>';
-            }
-            ?>
-        </tr>
+     <table>
+        <?php
+        $count = 0;
+        foreach ($keys as $key) {
+            if ($count % 3 === 0) echo "<tr>"; // nouvelle ligne toutes les 3 images
+            echo '<td><img src="image/' . $key . '" alt="' . $images[$key] . '" title="' . 
+            $images[$key] . '"></td>';
+            $count++;
+            if ($count % 3 === 0) echo "</tr>";
+        }
+        ?>
     </table>
 
     <a href="orderform.php">Passer votre commande</a>
